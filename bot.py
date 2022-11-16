6ba4b0af5d43f27c4f13268cdf96c3c0f4e1c07a
Pavel Shnyrin <pavel.shnyrin@jetbrains.com>
2022-11-16 18:13:06 +0100

    third

--- bot.py
+++ bot.py
@@ -18,26 +18,41 @@ def get_all():
 
     results = my_page["results"]
     prods = []
-    for result in results:
-        prods_dict = map_results(result)
+    for result in range(2, len(results)):
+        prods_dict = map_results(results[result])
         prods.append(prods_dict)
-    # pprint(prods)
+    pprint(prods)
 
 
 def map_results(result):
     # pprint(result)
-    i = 0
+    i = []
     prod_id = result["id"]
-    props = result["properties"]["Name"]["title"][i]
-    # name = props
-    pprint(props)
-    return prod_id
-    # name = props["Name"]["title"]["text"]["content"]
-    # return {
-    #     "prod_id": prod_id,
-    #     "name": name
-    #     }
+    name = result["properties"]["Name"]["title"][0]["text"]["content"]
+    return {
+        "prod_id": prod_id,
+        "name": name
+        }
+
+
+def get_all_to_buy():
+    my_page = notion.databases.query(
+        **{
+            "database_id": DB_ID,
+            "filter": { "property": "Available",
+                        "status": { "equals": "No",
+                        }
+            }
+        }
+    )
+
+    results = my_page["results"]
+    prods = []
+    for result in range(2, len(results)):
+        prods_dict = map_results(results[result])
+        prods.append(prods_dict)
+    pprint(prods)
 
 
 if __name__ == "__main__":
-    get_all()
+    get_all_to_buy()
